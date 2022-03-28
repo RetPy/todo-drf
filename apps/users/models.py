0e49c1914feb9fc320289a3742477bc2234a9db2
RetPy <shad02105@gmail.com>
2022-03-28 17:59:44 +0600

    AAAAAAH!!!

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,8 +1,51 @@
 from django.db import models
+from django.contrib.auth.base_user import BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.contrib.auth.validators import UnicodeUsernameValidator
 
 
-class User(models.Model):
+class UserManager(BaseUserManager):
+    def create_user(self, email, alias, password=None, is_admin=False, is_staff=False,
+                    is_active=True):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+        if not alias:
+            raise ValueError("User must have a alias")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.alias = alias
+        user.set_password(password)
+        user.admin = is_admin
+        user.staff = is_staff
+        user.active = is_active
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, alias, password=None, **extra_fields):
+        if not email:
+            raise ValueError("User must have an email")
+        if not password:
+            raise ValueError("User must have a password")
+        if not alias:
+            raise ValueError("User must have a alias")
+
+        user = self.model(
+            email=self.normalize_email(email)
+        )
+        user.alias = alias
+        user.set_password(password)
+        user.admin = True
+        user.staff = True
+        user.active = True
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractUser):
 
     username_validator = UnicodeUsernameValidator()
 
@@ -57,9 +100,11 @@ class User(models.Model):
         auto_now_add=True,
     )
 
-    @property
-    def is_authenticated(self):
-        return True
+    objects = UserManager()
+
+    USERNAME_FIELD = 'alias'
+
+    REQUIRED_FIELDS = ['firstName', 'lastName', 'email', 'direction', 'group', 'birthday', 'age']
 
     def __str__(self):
         return self.alias
